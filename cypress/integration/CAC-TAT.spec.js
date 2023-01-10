eb32b0f128060ef1344f4ead52d37b2618c5e7cf
prisfranca <105461578+prisfranca@users.noreply.github.com>
2023-01-10 00:19:42 -0300

    Aula9

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,11 +1,21 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+    beforeEach(function (){
         cy.visit('./src/index.html')
-
+    })
+    it('verifica o título da aplicação', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-  
+      })
+
+    it.only('preenche os campos obrigatórios e envia formulário', function(){
+        cy.get('#firstName').type('Priscila')
+        cy.get('#lastName').type('Alves França')
+        cy.get('#email').type('priscila@gmail.com')
+        cy.get('#open-text-area').type('teste')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success').should('be.visible')
     })
   })
   
